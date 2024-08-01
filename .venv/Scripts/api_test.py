ceca6c88dd4b67128f6a28ba92d8208b3e8d7268
prcp044-test <prashanthgowda044@gmail.com>
2024-07-31 21:43:48 -0700

    Update the code

--- .venv/Scripts/api_test.py
+++ .venv/Scripts/api_test.py
@@ -12,49 +12,43 @@ class Api():
     header={
         'accept': 'application / json'
     }
-    # url = get_config ()['API']['endpoint']
-    #
-    # response=api_request(url,"GET")
+    url = get_config ()['API']['endpoint']
+    response=api_request(url,"GET")
     # print(response.json())
-    # validate_status_code(response,200)
+    validate_status_code(response,200)
 
 # Send /post request with json body and validate response contains relevant data
-#     print(get_payload("prash@co.com","Prashanth",9620121212),"--------------")
 
-    # url = get_config ()['API']['endpoint-post']
-    #
-    # response = api_request ( url, "POST",headers=header,json=get_payload("prash@co.com","Prashanth",9620414242))
-    # res_json=response.json ()
-    # # print(res_json)
-    # validate_status_code ( response, 200 )
-    # validate_json_value(res_json,'custemail','prash@co.com')
-    # validate_json_value ( res_json, "custname", "Prashanth" )
-    # validate_json_value ( res_json, "custtel", 9620414242)
+    url = get_config ()['API']['endpoint-post']
+    response = api_request ( url, "POST",headers=header,json=get_payload("prash@co.com","Prashanth",9620414242))
+    res_json=response.json ()
+    validate_status_code ( response, 200 )
+    validate_json_value(res_json,'custemail','prash@co.com')
+    validate_json_value ( res_json, "custname", "Prashanth" )
+    validate_json_value ( res_json, "custtel", 9620414242)
 
 #Validate request with delayed response /delay/{delay_time}
 
 
-    # url = get_config ()['API']['endpoint-delay']
-    #
-    # response = api_request ( url, "GET")
-    # res_json=response.json ()
-    # # print(res_json)
-    # validate_status_code ( response, 200 )
-    # validate_json_delay_value(res_json,"url","https://httpbin.org/delay/5")
+    url = get_config ()['API']['endpoint-delay']
+    response = api_request ( url, "GET")
+    res_json=response.json ()
+    validate_status_code ( response, 200 )
+    validate_json_delay_value(res_json,"url","https://httpbin.org/delay/5")
 
 #Simulate Unauthorized Access
 
-    # url = get_config ()['API']['endpoint-auth']
-    # username = get_config ()['credentials']['UserName']
-    # password = get_config ()['credentials']['Password']
-    # response = api_request (url, "GET",auth=(username,password))
-    # validate_status_code ( response, 401 )
+    url = get_config ()['API']['endpoint-auth']
+    username = get_config ()['credentials']['UserName']
+    password = get_config ()['credentials']['Password']
+    response = api_request (url, "GET",auth=(username,password))
+    validate_status_code ( response, 401 )
 
 #Write any negative scenario (invalid URL)
 
     url = get_config ()['API']['endpoint-auth-neg']
     response = api_request ( url, "GET")
-    validate_status_code ( response, 401 )
+    validate_status_code ( response, 404 )
 
 
 
